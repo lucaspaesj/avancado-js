cccd6183c40bbc0566a2100b5730944b133cc415
lucaspaesj <94483135+lucaspaesj@users.noreply.github.com>
2021-12-02 20:07:58 -0300

    Add files via upload

---
+++ Aula5-testes/testes.js
@@ -0,0 +1,63 @@
+// Testes - Diversos tipos de abordagens para testes
+
+/** Testes automatizados:
+
+ * (Base): Testes unitários - Tem o papel de testar a menor unidade do código. Ex: Uma função/Um método/Uma classe;
+ * (Meio): Testes integrados - Testam a integração entre as pequenas partes que testamos anteriormente. Ex: Uma 
+ * função com outra função;
+ * (Topo): Testes funcionais(End to end) - Vai testar a integração do seu sistema com outros sistemas, vai testar
+ * uma funcionalidade de ponta a ponta.
+ **/
+
+/** Testes manuais e automatizados:
+
+ * Testes usabilidade;
+ * Testes de aceitação do usuário;
+ * Protótipos;
+ * Testes funcionais;
+ * Exemplos;
+ * Alpha e beta;
+ * Mais..
+ */
+
+/** Ferramentas de teste:
+
+ * Testes de carga e performance;
+ * Testes de segurança;
+ * Mais...
+ */
+
+// TDD (Test Driven Development) - Desenvolvimento orientado a testes:
+
+// Um dos pilares do Extreme Programming, consiste em testar e refatorar em pequenos ciclos
+// onde você escreve o teste antes do código, faz o mesmo passar e refatorar o código(Ajusta as partes
+// do código e o torna melhor).
+
+// Escrita do teste -> Escrita do código -> Refatoração
+
+// Vantagens:
+
+/**
+ * Feedback mais rápido;
+ * Maior segurança em alterações e novas funcionalidades;
+ * Código mais limpo;
+ * Produtividade.
+ */
+
+// BDD (Behavior Driven Develompent) - Desenvolvimento orientado a comportamento
+
+// Técnica de desenvolvimento ágil que visa integrar regras de negócio com linguagem de programação.
+
+/** Pilares:
+ * Testes;
+ * Documentação;
+ * Exemplos.
+ */
+
+// Vantagens:
+
+/**
+ * Compartilhamento de conhecimento;
+ * Documentação dinâmica;
+ * Visão do todo.
+ */
